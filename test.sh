963666be0307261a382428bb69e43cd42e31d3cd
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2023-11-12 17:39:13 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 ./target/release/polkadot --validator --name "ERN VENTURES" --chain=polkadot --database RocksDb --telemetry-url 'wss://telemetry-backend.w3f.community/submit 1' --prometheus-external --prometheus-port=9615
-^C
+Ctrl + c
