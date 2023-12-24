5fcc8368412de3653409fc0689d6d48fc92ea779
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2023-12-24 18:06:09 +0500

    Update installdotparity.sh

--- installdotparity.sh
+++ installdotparity.sh
@@ -193,32 +193,21 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] h
 
 sudo apt install -y protobuf-compiler
 
-git clone https://github.com/paritytech/polkadot.git
-
-cd polkadot
-git checkout v1.0.0
-
-./scripts/init.sh
-
-sudo apt install cmake -y
-
-rustup install nightly-2023-09-13
-
-rustup target add wasm32-unknown-unknown --toolchain nightly-2023-09-13
-
-cargo +nightly-2023-05-22 build --release
-
 cd
 
 git clone https://github.com/paritytech/polkadot-sdk.git 
 
 cd polkadot-sdk
  
-git checkout v1.4.0
+git checkout polkadot-v1.5.0
 
 ./scripts/init.sh
 
 sudo apt install cmake -y
+
+rustup component add rust-src
+
+rustup target add wasm32-unknown-unknown
 
 cargo +nightly-2023-09-13 build --release
 
