9878971e83dd254db907ad620c0beb2f287a0cb8
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-01-10 10:16:47 +0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,20 @@
 sudo git fetch
-sudo git checkout v1.4.0
-cargo +nightly-2023-09-13 build --release
+
+git checkout polkadot-v1.5.0
+
+rustup component add rust-src
+
+sudo apt install rustup component add rust-src -y
+
+rustup target add wasm32-unknown-unknown
+
+rustup install nightly-2023-05-22
+
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-05-22
+
+sudo apt-get install -y git clang curl make libssl-dev llvm libudev-dev protobuf-compiler
+
+rustup update
+
+cargo build --release
+
