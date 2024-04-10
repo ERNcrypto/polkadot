151205bff7a1eb24b9f3bda65ad0e4d77e54bbba
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-04-10 09:09:26 +0500

    Update installksmcurl.sh

--- installksmcurl.sh
+++ installksmcurl.sh
@@ -209,9 +209,9 @@ sudo apt install rustup component add rust-src -y
 
 rustup target add wasm32-unknown-unknown
 
-rustup install nightly-2023-05-22
+rustup install nightly-2024-01-21
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2023-05-22
+rustup target add wasm32-unknown-unknown --toolchain nightly-2024-01-21
 
 sudo apt-get install -y git clang curl make libssl-dev llvm libudev-dev protobuf-compiler
 
