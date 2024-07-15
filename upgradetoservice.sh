e5192e57634cabca91de02f17050852cfa05b12d
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-07-15 11:42:10 +0500

    Update upgradetoservice.sh

--- upgradetoservice.sh
+++ upgradetoservice.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 # Prompt the user for the node name
 read -p "Enter NODE name: " name
 echo "export NODE=$name"
 
-# Create the service file using the node name variable
+# Get the current username
+current_user=$(whoami)
+
+# Create the service file using the node name variable and current username
 sudo tee /etc/systemd/system/polkadot.service > /dev/null <<EOF
 [Unit]
 Description=Polkadot Validator Service
@@ -10,7 +15,7 @@ After=network.target
 
 [Service]
 Type=simple
-User=root
+User=$current_user
 ExecStart=$HOME/polkadot-sdk/target/release/polkadot --validator --name "$name" --chain=kusama --database RocksDb --telemetry-url 'wss://telemetry-backend.w3f.community/submit 1' --state-pruning 1000 --prometheus-external --prometheus-port=9615 --insecure-validator-i-know-what-i-do
 Restart=always
 RestartSec=10
