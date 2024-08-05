27dd7005e641d4f5d4d15e640c1b48e8040ea457
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-08-05 16:17:51 +0500

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -5,8 +5,6 @@ read -p "Enter IP server :" IP
 echo 'export IP='$IP
 read -p "TOKEN telegrambot:" TOKEN
 echo 'export TOKEN='$TOKEN
-read -p "Enter STARTNAME :" STARTNAME
-echo 'export STARTNAME='$STARTNAME
 
 sudo wget $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest |grep "tag_name" | awk '{print "https://github.com/prometheus/node_exporter/releases/download/" substr($2, 2, length($2)-3) "/node_exporter-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
 
