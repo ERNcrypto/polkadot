7a34cdd896bdbc60d646fe8ad42cbde9a8a9e3d5
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-08-05 16:21:08 +0500

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -6,13 +6,11 @@ echo 'export IP='$IP
 read -p "TOKEN telegrambot:" TOKEN
 echo 'export TOKEN='$TOKEN
 
-sudo wget $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest |grep "tag_name" | awk '{print "https://github.com/prometheus/node_exporter/releases/download/" substr($2, 2, length($2)-3) "/node_exporter-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
-
+# Установка node_exporter
+sudo wget $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "tag_name" | awk '{print "https://github.com/prometheus/node_exporter/releases/download/" substr($2, 2, length($2)-3) "/node_exporter-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
 sudo tar xvf node_exporter-*.tar.gz
 sudo cp ./node_exporter-*.linux-amd64/node_exporter /usr/local/bin/
-
 sudo useradd --no-create-home --shell /usr/sbin/nologin node_exporter
-
 sudo rm -rf ./node_exporter*
 
 sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOF
@@ -20,7 +18,7 @@ sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOF
   Description=Node Exporter
   Wants=network-online.target
   After=network-online.target
-[Service] 
+[Service]
   User=node_exporter
   Group=node_exporter
   Type=simple
@@ -33,20 +31,17 @@ sudo systemctl daemon-reload
 sudo systemctl start node_exporter.service
 sudo systemctl enable node_exporter.service
 
-sudo wget $(curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest |grep "tag_name" | awk '{print "https://github.com/prometheus/prometheus/releases/download/" substr($2, 2, length($2)-3) "/prometheus-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
-
+# Установка prometheus
+sudo wget $(curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep "tag_name" | awk '{print "https://github.com/prometheus/prometheus/releases/download/" substr($2, 2, length($2)-3) "/prometheus-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
 sudo tar xvf prometheus-*.tar.gz
 sudo cp ./prometheus-*.linux-amd64/prometheus /usr/local/bin/
-sudo cp ./prometheus-*.linux-amd64/promtool /usr/local/bin/ 
+sudo cp ./prometheus-*.linux-amd64/promtool /usr/local/bin/
 sudo cp -r ./prometheus-*.linux-amd64/consoles /etc/prometheus
 sudo cp -r ./prometheus-*.linux-amd64/console_libraries /etc/prometheus
-
 sudo useradd --no-create-home --shell /usr/sbin/nologin prometheus
 sudo mkdir /var/lib/prometheus
-
 sudo chown -R prometheus:prometheus /etc/prometheus
 sudo chown -R prometheus:prometheus /var/lib/prometheus
-
 sudo rm -rf ./prometheus*
 
 sudo tee /etc/prometheus/prometheus.yml > /dev/null <<EOF
@@ -84,7 +79,7 @@ sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
   --config.file /etc/prometheus/prometheus.yml \
   --storage.tsdb.path /var/lib/prometheus/ \
   --web.console.templates=/etc/prometheus/consoles \
-  --web.console.libraries=/etc/prometheus/console_libraries
+  --web.console.libraries=/etc/prometheus/console_libraries \
   --storage.tsdb.retention.time 30d \
   --web.enable-admin-api
   ExecReload=/bin/kill -HUP $MAINPID
@@ -93,27 +88,70 @@ sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
 EOF
 
 cd /etc/prometheus
+sudo tee rules.yml > /dev/null <<EOF
+groups:
+  - name: alert_rules
+    rules:
+      - alert: NodeDown
+        expr: up{job="kusama_node"} == 0
+        for: 1m
+        labels:
+          severity: critical
+        annotations:
+          summary: "Node $NODE down"
+          description: "Node $NODE has been down for more than 1 minute."
+      - alert: KusamaNodeSyncLag
+        expr: (max(chain_head_height{job="kusama_node"}) - max(chain_node_height{job="kusama_node"})) > 20
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: "Node $NODE lagging behind"
+          description: "Node $NODE is lagging more than 20 blocks behind the network."
+      - alert: HighDiskUsage
+        expr: (node_filesystem_avail_bytes{job="node_exporter",fstype!="tmpfs",fstype!="sysfs",fstype!="proc"} / node_filesystem_size_bytes{job="node_exporter",fstype!="tmpfs",fstype!="sysfs",fstype!="proc"}) * 100 < 5
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: "High disk usage on $NODE"
+          description: "Disk usage is above 95% on $NODE."
+      - alert: KusamaNodeNotSyncing
+        expr: sum(chain_is_syncing{job="kusama_node"}) == 0
+        for: 5m
+        labels:
+          severity: warning
+        annotations:
+          summary: "Node $NODE not syncing"
+          description: "Node $NODE is not syncing blocks for more than 5 minutes."
+      - alert: KusamaNodeHighCPUUsage
+        expr: rate(process_cpu_seconds_total{job="kusama_node"}[5m]) > 0.8
+        for: 5m
+        labels:
+          severity: warning
+        annotations:
+          summary: "High CPU usage on $NODE"
+          description: "CPU usage is above 80% on $NODE for more than 5 minutes."
+EOF
+
 sudo chown prometheus:prometheus rules.yml
 
 sudo systemctl daemon-reload
 sudo systemctl start prometheus.service
 sudo systemctl enable prometheus.service
 
+# Установка alertmanager
 cd ~
-sudo wget https://github.com/prometheus/alertmanager/releases/download/v0.24.0/alertmanager-0.24.0.linux-amd64.tar.gz;
+sudo wget https://github.com/prometheus/alertmanager/releases/download/v0.24.0/alertmanager-0.24.0.linux-amd64.tar.gz
 sudo tar xvf alertmanager-0.24.0.linux-amd64.tar.gz
 sudo rm alertmanager-0.24.0.linux-amd64.tar.gz
-
 sudo mkdir /etc/alertmanager /var/lib/prometheus/alertmanager
-
 cd alertmanager-0.24.0.linux-amd64
+sudo cp alertmanager amtool /usr/local/bin/
+sudo cp alertmanager.yml /etc/alertmanager
 
-sudo cp alertmanager amtool /usr/local/bin/ && sudo cp alertmanager.yml /etc/alertmanager
-
-useradd --no-create-home --shell /bin/false alertmanager
-
-sudo chown -R alertmanager:alertmanager /etc/alertmanager 
-/var/lib/prometheus/alertmanager
+sudo useradd --no-create-home --shell /bin/false alertmanager
+sudo chown -R alertmanager:alertmanager /etc/alertmanager /var/lib/prometheus/alertmanager
 sudo chown alertmanager:alertmanager /usr/local/bin/{alertmanager,amtool}
 
 sudo tee /etc/systemd/system/alertmanager.service > /dev/null <<EOF
@@ -149,37 +187,9 @@ inhibit_rules:
     equal: ['alertname', 'dev', 'instance']
 EOF
 
-sudo tee /etc/prometheus/rules.yml > /dev/null <<EOF
-groups:
-  - name: alert_rules
-    rules:
-      - alert: NodeDown
-        expr: up{job="kusama_node"} == 0
-        for: 1m
-        labels:
-          severity: critical
-        annotations:
-          summary: "Node $NODE down"
-          description: "Node $NODE has been down for more than 1 minute."
-      - alert: KusamaNodeSyncLag
-        expr: (max(chain_head_height{job="kusama_node"}) - max(chain_node_height{job="kusama_node"})) > 20
-        for: 5m
-        labels:
-          severity: critical
-        annotations:
-          summary: "Node $NODE lagging behind"
-          description: "Node $NODE is lagging more than 20 blocks behind the network."
-      - alert: HighDiskUsage
-        expr: (node_filesystem_avail_bytes{job="node_exporter",fstype!="tmpfs",fstype!="sysfs",fstype!="proc"} / node_filesystem_size_bytes{job="node_exporter",fstype!="tmpfs",fstype!="sysfs",fstype!="proc"}) * 100 < 5
-        for: 5m
-        labels:
-          severity: critical
-        annotations:
-          summary: "High disk usage on $NODE"
-          description: "Disk usage is above 95% on $NODE."
-EOF
-
-sudo systemctl daemon-reload && sudo systemctl enable alertmanager && sudo systemctl start alertmanager
+sudo systemctl daemon-reload
+sudo systemctl enable alertmanager
+sudo systemctl start alertmanager
 
 sudo systemctl restart prometheus.service
 sudo systemctl restart alertmanager
