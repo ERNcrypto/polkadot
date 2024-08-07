90ecc4b668dbdb8593b19f3a6888f7567b6c9f79
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-08-07 20:10:40 +0500

    Update token.sh

--- token.sh
+++ token.sh
@@ -6,10 +6,21 @@ CONFIG_FILE="/etc/alertmanager/alertmanager.yml"
 # Извлекаем токен из файла конфигурации
 TOKEN=$(grep -oP "(?<=/alertmanager/)[^']+" "$CONFIG_FILE")
 
+# Определяем IP-адрес машины
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+
 # Проверяем, удалось ли извлечь токен
 if [ -z "$TOKEN" ]; then
   echo "Токен не найден в файле конфигурации."
 else
   # Выводим токен в терминал
   echo "Извлеченный токен: $TOKEN"
+fi
+
+# Проверяем, удалось ли определить IP-адрес
+if [ -z "$IP_ADDRESS" ]; then
+  echo "Не удалось определить IP-адрес."
+else
+  # Выводим IP-адрес в терминал
+  echo "IP-адрес машины: $IP_ADDRESS"
 fi
