f8193c5393aa8b0bf5c07e73a0f2744bf2329954
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-08-11 14:33:05 +0500

    Create purgenew.sh

---
+++ purgenew.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+current_user=$(whoami)
+sudo systemctl stop polkadot.service
+
+cd polkadot-sdk
+
+./target/release/polkadot purge-chain --chain=kusama --database=RocksDb -y
+
+cd
+
+sudo rm -r polkadot-sdk
+
+git clone https://github.com/paritytech/polkadot-sdk.git 
+
+cd polkadot-sdk
+ 
+git checkout polkadot-v1.14.1
+
+cargo build --release
+
+sudo curl -o - -L https://snapshots.polkachu.com/snapshots/kusama/kusama_23934069.tar.lz4 | lz4 -c -d - | sudo tar -x -C /root/.local/share/polkadot/chains/ksmcc3/
+
+sudo chmod -R 777 /home/$current_user/.local/share/polkadot/chains/ksmcc3/db/full
+
+sudo systemctl daemon-reload
+
+sudo systemctl restart polkadot.service && sudo journalctl -u polkadot.service -f
