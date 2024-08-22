92f2d25abbae62634839f87ef943fbab2aa1a7c2
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-08-22 23:56:43 +0500

    Update rootinstallksm.sh

--- rootinstallksm.sh
+++ rootinstallksm.sh
@@ -296,7 +296,7 @@ After=network.target
 [Service]
 Type=simple
 User=$current_user
-ExecStart=root/polkadot-sdk/target/release/polkadot --validator --name "$STARTNAME1" --chain=kusama --database RocksDb --telemetry-url 'wss://telemetry-backend.w3f.community/submit 1' --state-pruning 1000 --prometheus-external --prometheus-port=9615 --insecure-validator-i-know-what-i-do
+ExecStart=$HOME/polkadot-sdk/target/release/polkadot --validator --name "$STARTNAME1" --chain=kusama --database RocksDb --telemetry-url 'wss://telemetry-backend.w3f.community/submit 1' --state-pruning 1000 --prometheus-external --prometheus-port=9615 --insecure-validator-i-know-what-i-do
 Restart=always
 RestartSec=10
 StandardOutput=journal
