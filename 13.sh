412b5bc5523f64636d136b96e787797f5fd9937b
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-09-15 01:29:35 +0500

    Update 13.sh

--- 13.sh
+++ 13.sh
@@ -1,1 +1,6 @@
-#
+#!/bin/bash
+curl -o - -L https://snapshots.polkachu.com/snapshots/kusama/kusama_23934069.tar.lz4 | lz4 -c -d - | tar -x -C /root/.local/share/polkadot/chains/ksmcc3/
+
+sudo chmod -R 777 /home/admuser/.local/share/polkadot/chains/ksmcc3/db/full
+
+sudo systemctl restart polkadot.service && sudo journalctl -u polkadot.service -f
