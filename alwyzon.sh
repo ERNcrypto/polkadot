a15f24704b5b43aca3453fca3392c84e12d73582
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-09-28 00:04:05 +0500

    Create alwyzon.sh

---
+++ alwyzon.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sudo systemctl stop polkadot.service
+
+cd polkadot-sdk
+
+./target/release/polkadot purge-chain --chain=kusama --database=RocksDb -y
+
+cd
+
+sudo rm -r polkadot-sdk
+
+git clone https://github.com/paritytech/polkadot-sdk.git 
+
+cd polkadot-sdk
+ 
+git checkout polkadot-v1.16.0
+
+cargo build --release
+
+sudo chmod -R 777 /home/alwyzon/.local/share/polkadot/chains/ksmcc3/db/full
+
+sudo chmod -R 777 /home/alwyzon/polkadot-sdk
+
+sudo systemctl start polkadot.service
+
+sleep 450
+
+sudo systemctl stop polkadot.service
+
+./target/release/polkadot purge-chain --chain=kusama --database=RocksDb -y
+
+
+sudo curl -o - -L https://kusama.services-ernventures.com/kusama/snap_kusama.tar.lz4 | lz4 -c -d - | sudo tar -x -C /home/alwyzon/.local/share/polkadot/chains/ksmcc3/
+
+sudo chmod -R 777 /home/alwyzon/.local/share/polkadot/chains/ksmcc3/db/full
+
+sudo systemctl daemon-reload
+
+sudo systemctl restart polkadot.service && sudo journalctl -u polkadot.service -f
