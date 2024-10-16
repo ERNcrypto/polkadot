f22dc04683a9864e80e683b24b8d599e985465be
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-10-16 15:02:00 +0500

    Update installdotcurl.sh

--- installdotcurl.sh
+++ installdotcurl.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+# Get the current username
+current_user=$(whoami)
 read -p "Enter NODE name:" NODE
 echo 'export NODE='$NODE
-read -p "Enter IP server :" IP
-echo 'export IP='$IP
+read -p "Enter IP server :" IP_ADDRESS
+echo 'export IP='$IP_ADDRESS
 read -p "TOKEN telegrambot:" TOKEN
 echo 'export TOKEN='$TOKEN
 read -p "Enter STARTNAME :" STARTNAME
 echo 'export STARTNAME='$STARTNAME
+read -p "Enter STARTNAME :" STARTNAME1
+echo 'export STARTNAME='$STARTNAME1
 
-wget $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest |grep "tag_name" | awk '{print "https://github.com/prometheus/node_exporter/releases/download/" substr($2, 2, length($2)-3) "/node_exporter-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
-
-tar xvf node_exporter-*.tar.gz
+# Установка node_exporter
+sudo wget $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "tag_name" | awk '{print "https://github.com/prometheus/node_exporter/releases/download/" substr($2, 2, length($2)-3) "/node_exporter-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
+sudo tar xvf node_exporter-*.tar.gz
 sudo cp ./node_exporter-*.linux-amd64/node_exporter /usr/local/bin/
-
 sudo useradd --no-create-home --shell /usr/sbin/nologin node_exporter
-
-rm -rf ./node_exporter*
+sudo rm -rf ./node_exporter*
 
 sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOF
 [Unit]
@@ -35,21 +37,18 @@ sudo systemctl daemon-reload
 sudo systemctl start node_exporter.service
 sudo systemctl enable node_exporter.service
 
-wget $(curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest |grep "tag_name" | awk '{print "https://github.com/prometheus/prometheus/releases/download/" substr($2, 2, length($2)-3) "/prometheus-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
-
-tar xvf prometheus-*.tar.gz
+# Установка prometheus
+sudo wget $(curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep "tag_name" | awk '{print "https://github.com/prometheus/prometheus/releases/download/" substr($2, 2, length($2)-3) "/prometheus-" substr($2, 3, length($2)-4) ".linux-amd64.tar.gz"}')
+sudo tar xvf prometheus-*.tar.gz
 sudo cp ./prometheus-*.linux-amd64/prometheus /usr/local/bin/
-sudo cp ./prometheus-*.linux-amd64/promtool /usr/local/bin/ 
+sudo cp ./prometheus-*.linux-amd64/promtool /usr/local/bin/
 sudo cp -r ./prometheus-*.linux-amd64/consoles /etc/prometheus
 sudo cp -r ./prometheus-*.linux-amd64/console_libraries /etc/prometheus
-
 sudo useradd --no-create-home --shell /usr/sbin/nologin prometheus
 sudo mkdir /var/lib/prometheus
-
 sudo chown -R prometheus:prometheus /etc/prometheus
 sudo chown -R prometheus:prometheus /var/lib/prometheus
-
-rm -rf ./prometheus*
+sudo rm -rf ./prometheus*
 
 sudo tee /etc/prometheus/prometheus.yml > /dev/null <<EOF
 global:
@@ -61,12 +60,16 @@ alerting:
   alertmanagers:
   - static_configs:
     - targets:
-      - $IP:9093
+      - $IP_ADDRESS:9093
 scrape_configs:
-  - job_name: "$NODE"
+  - job_name: "node_exporter"
     scrape_interval: 5s
     static_configs:
-      - targets: ["$IP:9090","$IP:9615"]
+      - targets: ["$IP_ADDRESS:9100"]
+  - job_name: "kusama_node"
+    scrape_interval: 5s
+    static_configs:
+      - targets: ["$IP_ADDRESS:9615"]
 EOF
 
 sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
@@ -82,7 +85,7 @@ sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
   --config.file /etc/prometheus/prometheus.yml \
   --storage.tsdb.path /var/lib/prometheus/ \
   --web.console.templates=/etc/prometheus/consoles \
-  --web.console.libraries=/etc/prometheus/console_libraries
+  --web.console.libraries=/etc/prometheus/console_libraries \
   --storage.tsdb.retention.time 30d \
   --web.enable-admin-api
   ExecReload=/bin/kill -HUP $MAINPID
@@ -91,28 +94,71 @@ sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
 EOF
 
 cd /etc/prometheus
+sudo tee rules.yml > /dev/null <<EOF
+groups:
+  - name: alert_rules
+    rules:
+      - alert: KusamaNodeSyncLag
+        expr: (max(substrate_block_height{status="best"}) by (instance) - max(substrate_block_height{status="finalized"}) by (instance)) > 20
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: "Node $NODE lagging behind"
+          description: "Node $NODE is lagging more than 20 blocks behind the network."
+      - alert: NodeDown
+        expr: up{job="kusama_node"} == 0
+        for: 1m
+        labels:
+          severity: critical
+        annotations:
+          summary: "Node $NODE down"
+          description: "Node $NODE has been down for more than 1 minute."
+      - alert: HighDiskUsage
+        expr: (node_filesystem_avail_bytes{job="node_exporter", fstype!="tmpfs", fstype!="sysfs", fstype!="proc"} / node_filesystem_size_bytes{job="node_exporter", fstype!="tmpfs", fstype!="sysfs", fstype!="proc"}) * 100 < 2
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: "High disk usage on $NODE"
+          description: "Disk usage is above 98% on $NODE."
+      - alert: KusamaNodeNotSyncing
+        expr: substrate_sub_libp2p_sync_is_major_syncing{job="kusama_node"} == 1
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: "Node $NODE not syncing"
+          description: "Node $NODE is not syncing blocks for more than 5 minutes."
+      - alert: KusamaNodeHighCPUUsage
+        expr: rate(process_cpu_seconds_total{job="kusama_node"}[5m]) > 0.8
+        for: 5m
+        labels:
+          severity: warning
+        annotations:
+          summary: "High CPU usage on $NODE"
+          description: "CPU usage is above 80% on $NODE for more than 5 minutes."
+EOF
+
 sudo chown prometheus:prometheus rules.yml
 
 sudo systemctl daemon-reload
 sudo systemctl start prometheus.service
 sudo systemctl enable prometheus.service
 
+# Установка alertmanager
 cd ~
-wget https://github.com/prometheus/alertmanager/releases/download/v0.24.0/alertmanager-0.24.0.linux-amd64.tar.gz;
-tar xvf alertmanager-0.24.0.linux-amd64.tar.gz
-rm alertmanager-0.24.0.linux-amd64.tar.gz
-
-mkdir /etc/alertmanager /var/lib/prometheus/alertmanager
-
+sudo wget https://github.com/prometheus/alertmanager/releases/download/v0.24.0/alertmanager-0.24.0.linux-amd64.tar.gz
+sudo tar xvf alertmanager-0.24.0.linux-amd64.tar.gz
+sudo rm alertmanager-0.24.0.linux-amd64.tar.gz
+sudo mkdir /etc/alertmanager /var/lib/prometheus/alertmanager
 cd alertmanager-0.24.0.linux-amd64
+sudo cp alertmanager amtool /usr/local/bin/
+sudo cp alertmanager.yml /etc/alertmanager
 
-cp alertmanager amtool /usr/local/bin/ && cp alertmanager.yml /etc/alertmanager
-
-useradd --no-create-home --shell /bin/false alertmanager
-
-chown -R alertmanager:alertmanager /etc/alertmanager 
-/var/lib/prometheus/alertmanager
-chown alertmanager:alertmanager /usr/local/bin/{alertmanager,amtool}
+sudo useradd --no-create-home --shell /bin/false alertmanager
+sudo chown -R alertmanager:alertmanager /etc/alertmanager /var/lib/prometheus/alertmanager
+sudo chown alertmanager:alertmanager /usr/local/bin/{alertmanager,amtool}
 
 sudo tee /etc/systemd/system/alertmanager.service > /dev/null <<EOF
 [Unit]
@@ -123,7 +169,7 @@ After=network-online.target
 User=root
 Group=root
 Type=simple
-ExecStart=/usr/local/bin/alertmanager --config.file /etc/alertmanager/alertmanager.yml --web.external-url=http://$IP:9093 --cluster.advertise-address='0.0.0.0:9093'
+ExecStart=/usr/local/bin/alertmanager --config.file /etc/alertmanager/alertmanager.yml --web.external-url=http://$IP_ADDRESS:9093 --cluster.advertise-address='0.0.0.0:9093'
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -147,24 +193,12 @@ inhibit_rules:
     equal: ['alertname', 'dev', 'instance']
 EOF
 
-sudo tee /etc/prometheus/rules.yml > /dev/null <<EOF
-groups:
-  - name: alert_rules
-    rules:
-      - alert: $NODE
-        expr: up == 0
-        for: 1m
-        labels:
-          severity: critical
-        annotations:
-          summary: "$NODE"
-          description: "Node has been down for more than 1 minute."          
-EOF
-
-sudo systemctl daemon-reload && sudo systemctl enable alertmanager && sudo systemctl start alertmanager
+sudo systemctl daemon-reload
+sudo systemctl enable alertmanager
+sudo systemctl start alertmanager
 
 sudo systemctl restart prometheus.service
-sudo systemctl restart alertmanager
+sudo systemctl restart alertmanager.service
 
 cd
 sudo apt install curl -y
@@ -199,7 +233,7 @@ git clone https://github.com/paritytech/polkadot-sdk.git
 
 cd polkadot-sdk
  
-git checkout polkadot-v1.14.1
+git checkout polkadot-v1.16.0
 
 sudo apt install cmake -y
 
@@ -209,9 +243,9 @@ sudo apt install rustup component add rust-src -y
 
 rustup target add wasm32-unknown-unknown
 
-rustup install nightly-2023-05-22
+rustup install nightly-2024-01-21
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2023-05-22
+rustup target add wasm32-unknown-unknown --toolchain nightly-2024-01-21
 
 sudo apt-get install -y git clang curl make libssl-dev llvm libudev-dev protobuf-compiler
 
@@ -221,4 +255,70 @@ cargo update -p time
 
 cargo build --release
 
-./target/release/polkadot --validator --name "$STARTNAME" --chain=polkadot --database RocksDb --telemetry-url 'wss://telemetry-backend.w3f.community/submit 1' --state-pruning 1000 --prometheus-external --prometheus-port=9615 --insecure-validator-i-know-what-i-do --unsafe-force-node-key-generation
+sudo chmod -R 777 /home/$current_user/polkadot-sdk
+
+sudo chmod -R 777 /home/$current_user/.local/share/polkadot/chains/polkadot/db/full
+
+sudo chmod -R 777 /home/$current_user/.local/share/polkadot/chains/polkadot/
+
+# Create the service file using the node name variable and current username
+sudo tee /etc/systemd/system/polkadot.service > /dev/null <<EOF
+[Unit]
+Description=Polkadot Validator Service
+After=network.target
+
+[Service]
+Type=simple
+User=$current_user
+ExecStart=$HOME/polkadot-sdk/target/release/polkadot --validator --name "$STARTNAME" --chain=polkadot --database RocksDb --telemetry-url 'wss://telemetry-backend.w3f.community/submit 1' --state-pruning 1000 --prometheus-external --prometheus-port=9615 --insecure-validator-i-know-what-i-do --unsafe-force-node-key-generation
+Restart=always
+RestartSec=10
+StandardOutput=journal
+StandardError=journal
+Environment=START
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd daemon and start the service
+sudo systemctl daemon-reload
+sudo systemctl enable polkadot.service
+sudo systemctl restart polkadot.service
+
+sleep 600
+
+sudo systemctl stop polkadot.service
+
+./target/release/polkadot purge-chain --chain=polkadot --database=RocksDb -y
+
+sudo curl -o - -L https://snapshots.radiumblock.com/polkadot_6247335_2024-10-16.tar.lz4 | lz4 -c -d - | sudo tar -x -C /home/$current_user/.local/share/polkadot/chains/polkadot/
+
+sudo chmod -R 777 /home/$current_user/polkadot-sdk
+
+sudo chmod -R 777 /home/$current_user/.local/share/polkadot/chains/polkadot/db/full
+
+sudo chmod -R 777 /home/$current_user/.local/share/polkadot/chains/polkadot/
+
+sudo tee /etc/systemd/system/polkadot.service > /dev/null <<EOF
+[Unit]
+Description=Polkadot Validator Service
+After=network.target
+
+[Service]
+Type=simple
+User=$current_user
+ExecStart=$HOME/polkadot-sdk/target/release/polkadot --validator --name "$STARTNAME1" --chain=polkadot --database RocksDb --telemetry-url 'wss://telemetry-backend.w3f.community/submit 1' --state-pruning 1000 --prometheus-external --prometheus-port=9615 --insecure-validator-i-know-what-i-do
+Restart=always
+RestartSec=10
+StandardOutput=journal
+StandardError=journal
+Environment=START
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+
+sudo systemctl restart polkadot.service && sudo journalctl -u polkadot.service -f
