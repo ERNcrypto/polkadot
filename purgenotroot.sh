ac2545f17cdfdcf0411a087dd33515edc3873b09
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-11-06 12:36:20 +0500

    Update purgenotroot.sh

--- purgenotroot.sh
+++ purgenotroot.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
+
+read -sp "Enter password for sudo: " sudo_password
+echo
+
+echo $sudo_password | sudo -S systemctl stop polkadot.service
+
 current_user=$(whoami)
-sudo systemctl stop polkadot.service
-
-cd polkadot-sdk
-
-./target/release/polkadot purge-chain --chain=kusama --database=RocksDb -y
 
 cd
-
-sudo rm -r polkadot-sdk
-
-git clone https://github.com/paritytech/polkadot-sdk.git 
+echo $sudo_password | sudo -S rm -r polkadot-sdk
+git clone https://github.com/paritytech/polkadot-sdk.git
 
 cd polkadot-sdk
- 
-git checkout polkadot-v1.16.0
-
+git checkout polkadot-v1.16.1
 cargo build --release
 
-sudo chmod -R 777 /home/$current_user/.local/share/polkadot/chains/ksmcc3/db/full
+echo $sudo_password | sudo -S chmod -R 777 /home/$current_user/.local/share/polkadot/chains/ksmcc3/db/full
+echo $sudo_password | sudo -S chmod -R 777 /home/$current_user/polkadot-sdk
 
-sudo chmod -R 777 /home/$current_user/polkadot-sdk
-
-sudo systemctl start polkadot.service
-
-sleep 450
-
-sudo systemctl stop polkadot.service
-
-./target/release/polkadot purge-chain --chain=kusama --database=RocksDb -y
-
-
-sudo curl -o - -L https://kusama.services-ernventures.com/kusama/snap_kusama.tar.lz4 | lz4 -c -d - | sudo tar -x -C /home/$current_user/.local/share/polkadot/chains/ksmcc3/
-
-sudo chmod -R 777 /home/$current_user/.local/share/polkadot/chains/ksmcc3/db/full
-
-sudo systemctl daemon-reload
-
-sudo systemctl restart polkadot.service && sudo journalctl -u polkadot.service -f
+echo $sudo_password | sudo -S systemctl restart polkadot.service
+echo $sudo_password | sudo -S journalctl -u polkadot.service -f
